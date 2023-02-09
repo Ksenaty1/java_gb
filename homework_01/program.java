899e00270a75841b3dbf672294f88fb6c713c0d6
Yaroslav <yarlay.learning@gmail.com>
2023-02-09 15:40:02 +0300

    added Homework 1

---
+++ homework_01/program.java
@@ -0,0 +1,16 @@
+package homework_01;
+
+public class program {
+    public static void main(String[] args) {
+        
+        // Ввод 
+        int n = 4;
+
+        // Вычисление треугольного числа
+        int triangular_number = (int) (0.5 * n * (n + 1));
+
+        // Вывод
+        System.out.println(triangular_number);
+        
+    }
+}
